79c82c3fb85f19205c97487a1cecd255ba901fed
gichuru <gichuru@gmail.com>
2017-02-02 10:15:08 +0300

    the fuckup predicted ealier happenned - set region, let see if it works

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
-    :aws_secret_access_key  => ENV["aws_secret_access_key"],                        # required
+    :aws_secret_access_key  => ENV["aws_secret_access_key"],
+    :region                 => 'eu-west-2' # 
   }
   config.fog_directory  = ENV["fog_directory"]                    # required
 end

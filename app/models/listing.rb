48d43d9804ef6d21ff5b33de75d1857b3c912b2e
gichuru <gichuru@gmail.com>
2017-02-08 09:20:56 +0300

    the listing file ordering by latest

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ApplicationRecord
     mount_uploader :image, ImageUploader
     has_many :reviews
+    has_many :users, through: :reviews
     validates :name, :address, :phone, :email, :website, :image, presence: true
+    Listing.order("created_at DESC")
 end

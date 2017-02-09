11fe20d4c7bab9565216198f32d3f675c8a613a0
gichuru <gichuru@gmail.com>
2017-02-09 09:24:24 +0300

    removed upvote/downvote on set review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user! 
   before_action :set_listing
-  before_action :set_review, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :update, :destroy]
 
 

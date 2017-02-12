8fe946a9c912cbdf62456efa7ee201407bbce379
gichuru <gichuru@gmail.com>
2017-02-12 12:43:39 +0300

    reodering

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,6 @@ class Listing < ApplicationRecord
     mount_uploader :image, ImageUploader
     has_many :reviews 
     validates :name, :address, :phone, :email, :website, :image, presence: true
-    #Listing.order("created_at DESC")
+    Listing.order("created_at ASCE")
     has_many :users, through: :review
 end

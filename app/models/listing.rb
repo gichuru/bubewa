ab6b96b512055ae594841203c1d31c8bd64cb53a
gichuru <gichuru@gmail.com>
2017-02-20 09:21:54 +0300

    changed posted to requested

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,9 @@
 class Listing < ApplicationRecord
+	
     mount_uploader :image, ImageUploader
     has_many :reviews 
     validates :name, :address, :phone, :email, :website, :image, presence: true
-    #Listing.order("name_by ASC")
+    scope :ordered_listings, -> { order(listings: :asc) }
     has_many :users, through: :review
     belongs_to :category
 end

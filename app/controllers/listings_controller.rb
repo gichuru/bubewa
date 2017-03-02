f51f7295e5dcd50ad702bebd562c13af3e390c58
gichuru <gichuru@gmail.com>
2017-03-02 20:29:07 +0300

    pagination numbers and brand text

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,10 +7,10 @@ class ListingsController < ApplicationController
   # GET /listings.json
   def index
     if params[:category].blank?
-      @listings = Listing.all.paginate(:page => params[:page], :per_page => 8)
+      @listings = Listing.all.paginate(:page => params[:page], :per_page => 12)
     else
       @category_id = Category.find(params[:category]).id
-      @listings = Listing.where(category_id: @category_id).order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+      @listings = Listing.where(category_id: @category_id).order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
     end
   end
 

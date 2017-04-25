27c13f4ebac37cce288bbb599f41f1e181dfdb87
Samuel Ralak <samuelralak@hotmail.com>
2017-04-25 17:51:51 +0300

    added og properties and twiiter cards (SEO)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def page_title(page_title)
+		base_title = "Kenyan ratings and reviews community, products, services, public servants and institutions"
+
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title}"
+		end
+	end
 end
